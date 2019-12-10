eea0d6d1f17dfe237f938e9d38e30e072855cd36
Frank Wang <FrankWangSydney@gmail.com>
2019-12-10 22:03:21 +1100

    Added database and notification ports

--- src/Domain/Auth.hs
+++ src/Domain/Auth.hs
@@ -2,8 +2,9 @@ module Domain.Auth where
     
 import ClassyPrelude
 import Domain.Validation
+import Control.Monad.Except
 
-newtype Email = Email {emailRaw::Text} deriving (Show, Eq)
+newtype Email = Email {emailRaw :: Text} deriving (Show, Eq)
 
 rawEmail :: Email -> Text
 rawEmail = emailRaw
@@ -13,7 +14,7 @@ data EmailValidationErr = EmailValidationErrInvalidEmail
 mkEmail :: Text -> Either [Text] Email
 mkEmail val = Right $ Email val
 
-newtype Password = Password { passwordRaw::Text} deriving (Show, Eq)
+newtype Password = Password { passwordRaw :: Text} deriving (Show, Eq)
 
 rawPassword :: Password -> Text
 rawPassword = passwordRaw
@@ -33,3 +34,20 @@ data Auth = Auth
   { authEmail :: Email
   , authPassword :: Password  
   } deriving (Show, Eq)
+
+type VerificationCode = Text
+
+data RegistrationError = RegistrationErrorEmailTaken 
+  deriving (Show, Eq)
+
+class Monad m => AuthRepo m where
+  addAuth :: Auth -> m (Either RegistrationError VerificationCode)
+
+class Monad m => EmailVerificationNotif m where
+  notifyEmailVerification :: Email -> VerificationCode -> m ()
+
+register :: (AuthRepo m, EmailVerificationNotif m) => Auth -> m (Either RegistrationError ())
+register auth = runExceptT $ do
+  vCode <- ExceptT $ addAuth auth
+  let email = authEmail auth
+  lift $ notifyEmailVerification email vCode
