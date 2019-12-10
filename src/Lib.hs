263d5f6b735b528fb7cfcb7fcf61ad6f8fb22f39
Frank Wang <FrankWangSydney@gmail.com>
2019-12-11 07:38:11 +1100

    Added fake implementation for repo

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,6 +3,16 @@ module Lib
     ) where
 
 import ClassyPrelude
+import Domain.Auth
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+instance AuthRepo IO where
+    addAuth (Auth email pass) = do
+        putStrLn $ "adding auth: " <> rawEmail email
+        return $ Right "fake verification code"
+
+instance EmailVerificationNotif IO where
+    notifyEmailVerification email vcode = 
+        putStrLn $ "Notify " <> rawEmail email <> " - " <> vcode
