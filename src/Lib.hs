7441b3b8d9b138bfcda2180729869448390444bc
Frank Wang <Frank.Wang@servcorp.com.au>
2019-12-13 17:09:22 +1100

    Added one monad transformer for app

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,17 +4,29 @@ module Lib
 
 import ClassyPrelude
 import Domain.Auth
+import qualified Adapter.InMemory.Auth as M
+import Control.Monad.Reader
+
+type State = TVar M.State
+newtype App a = App
+    { unApp :: ReaderT State IO a        
+    } deriving (Applicative, Functor, Monad, MonadReader State, MonadIO)
+
+run :: State -> App a -> IO a
+run state = flip runReaderT state . unApp    
+
+instance AuthRepo App where
+    addAuth = M.addAuth
+    setEmailAsVerified = M.setEmailAsVerified
+    findUserByAuth = M.findUserByAuth
+    findEmailFromUserId = M.findEmailFromUserId
+
+instance EmailVerificationNotif App where
+    notifyEmailVerification = M.notifyEmailVerification
+
+instance SessionRepo App where
+    newSession = M.newSession
+    findUserIdBySessionId = M.findUserIdBySessionId
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
-
--- instance AuthRepo IO where
---     addAuth (Auth email pass) = do
---         putStrLn $ "adding auth: " <> rawEmail email
---         return $ Right "fake verification code"
-
--- instance EmailVerificationNotif IO where
---     notifyEmailVerification email vcode = 
---         putStrLn $ "Notify " <> rawEmail email <> " - " <> vcode
-
-
