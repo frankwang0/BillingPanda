caf5a612295486c2de899ddf49c92ca4c64b9b9a
Frank Wang <FrankWangSydney@gmail.com>
2019-12-13 23:18:15 +1100

    Added -threaded option

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,48 +1,47 @@
 module Lib
-    ( someFunc
-    ) where
+  ( someFunc
+  ) where
 
 import ClassyPrelude
-import Domain.Auth
 import qualified Adapter.InMemory.Auth as M
+import Domain.Auth
 import Control.Monad.Reader
 
 type State = TVar M.State
 newtype App a = App
-    { unApp :: ReaderT State IO a        
-    } deriving (Applicative, Functor, Monad, MonadReader State, MonadIO)
+  { unApp :: ReaderT State IO a
+  } deriving (Applicative, Functor, Monad, MonadReader State, MonadIO)
 
 run :: State -> App a -> IO a
-run state = flip runReaderT state . unApp    
+run state = flip runReaderT state . unApp
 
 instance AuthRepo App where
-    addAuth = M.addAuth
-    setEmailAsVerified = M.setEmailAsVerified
-    findUserByAuth = M.findUserByAuth
-    findEmailFromUserId = M.findEmailFromUserId
+  addAuth = M.addAuth
+  setEmailAsVerified = M.setEmailAsVerified
+  findUserByAuth = M.findUserByAuth
+  findEmailFromUserId = M.findEmailFromUserId
 
 instance EmailVerificationNotif App where
-    notifyEmailVerification = M.notifyEmailVerification
+  notifyEmailVerification = M.notifyEmailVerification
 
 instance SessionRepo App where
-    newSession = M.newSession
-    findUserIdBySessionId = M.findUserIdBySessionId
+  newSession = M.newSession
+  findUserIdBySessionId = M.findUserIdBySessionId
 
 someFunc :: IO ()
 someFunc = do
-    state <- newTVarIO M.initialState
-    run state action
+  state <- newTVarIO M.initialState
+  run state action
 
 action :: App ()
 action = do
-    let email = either undefined id $ mkEmail "frank.wang@servcorp.com.au"
-        passw = either undefined id $ mkPassword "Password1"
-        auth = Auth email passw
-
-    register auth
-    Just vCode <- M.getNotificationForEmail email
-    verifyEmail vCode
-    Right session <- login auth
-    Just uId <- resolveSessionId session
-    Just registeredEmail <- getUser uId
-    print (session, uId, registeredEmail)
+  let email = either undefined id $ mkEmail "ecky@test.com"
+      passw = either undefined id $ mkPassword "1234ABCDefgh"
+      auth = Auth email passw
+  register auth
+  -- Just vCode <- M.getNotificationForEmail email
+--   verifyEmail vCode
+--   Right session <- login auth
+--   Just uId <- resolveSessionId session
+--   registeredEmail <- getUser uId
+  print ("", 0, email)
