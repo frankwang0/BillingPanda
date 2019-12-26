2f387e13b6f633b30550022cb8306352983c4a56
Frank Wang <FrankWangSydney@gmail.com>
2019-12-27 09:57:07 +1100

    Finished RabbitMQ implementation

--- app/Main.hs
+++ app/Main.hs
@@ -8,11 +8,13 @@ import Control.Monad.Reader
 import Control.Monad.Fail
 import qualified Control.Monad.Catch as E
 import qualified Adapter.Redis.Auth as Redis
+import qualified Adapter.RabbitMQ.Common as MQ
+import qualified Adapter.RabbitMQ.Auth as MQAuth
 
-type State = (PG.State, Redis.State, TVar M.State)
+type State = (PG.State, Redis.State, MQ.State, TVar M.State)
 newtype App a = App
     { unApp :: ReaderT State IO a
-    } deriving (Applicative, Functor, Monad, MonadReader State, MonadIO, MonadFail, E.MonadThrow)
+    } deriving (Applicative, Functor, Monad, MonadReader State, MonadIO, MonadFail, E.MonadThrow, MonadUnliftIO)
 
 run :: State -> App a -> IO a
 run state = flip runReaderT state . unApp
@@ -24,7 +26,7 @@ instance AuthRepo App where
     findEmailFromUserId = PG.findEmailFromUserId
 
 instance EmailVerificationNotif App where
-    notifyEmailVerification = M.notifyEmailVerification
+    notifyEmailVerification = MQAuth.notifyEmailVerification
 
 instance SessionRepo App where
     newSession = Redis.newSession
@@ -32,7 +34,7 @@ instance SessionRepo App where
 
 action :: App ()
 action = do
-    let email = either undefined id $ mkEmail "cleancodematters@gmail.com"
+    let email = either undefined id $ mkEmail "cleancodematters1@gmail.com"
         passw = either undefined id $ mkPassword "Password1"
         auth = Auth email passw
     register auth
@@ -48,8 +50,12 @@ main = do
     mState <- newTVarIO M.initialState
     PG.withState pgCfg $ \pgState ->
       Redis.withState redisCfg $ \redisState ->
-        run (pgState, redisState, mState) action
+        MQ.withState mqCfg 16 $ \ mqState -> do
+            let runner = run (pgState, redisState, mqState, mState)
+            MQAuth.init mqState runner
+            runner action
     where
+      mqCfg = "amqp://guest:guest@localhost:5672/%2F"
       redisCfg = "redis://localhost:6379/0"
       pgCfg = PG.Config 
               { PG.configUrl = "postgresql://localhost/hauth"
